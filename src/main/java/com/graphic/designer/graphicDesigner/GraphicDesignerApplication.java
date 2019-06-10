5035ba8779427c6bf06db79aa39f6cfdd4e861ef
Mateusz Partyka <mateuszpartyka12@gmail.com>
2019-06-10 17:04:26 +0200

    first commit

---
+++ src/main/java/com/graphic/designer/graphicDesigner/GraphicDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.graphic.designer.graphicDesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphicDesignerApplication.class, args);
+	}
+
+}
