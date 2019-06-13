fb07fd63173bac626dee93171b1c4fa19852a1e7
Mateusz Partyka <mateuszpartyka12@gmail.com>
2019-06-13 19:55:24 +0200

    security config

--- src/main/java/com/graphic/designer/graphicDesigner/GraphicDesignerApplication.java
+++ src/main/java/com/graphic/designer/graphicDesigner/GraphicDesignerApplication.java
@@ -2,6 +2,9 @@ package com.graphic.designer.graphicDesigner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GraphicDesignerApplication {
@@ -10,4 +13,8 @@ public class GraphicDesignerApplication {
 		SpringApplication.run(GraphicDesignerApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
